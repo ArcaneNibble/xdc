d8b19955fe141a5343bedc078f560bfae06ab98e
R <rqou@berkeley.edu>
2023-07-02 16:39:58 +0100

    make tests actually #[test]

--- xdc_test/src/main.rs
+++ xdc_test/src/main.rs
@@ -20,6 +20,11 @@ trait HasColor: HasId {
     fn color(&self) -> u32;
 }
 
+#[xdc_trait]
+trait HasTaste {
+    fn taste(&self) -> &'static str;
+}
+
 #[xdc_struct]
 struct Point {
     id: u32,
@@ -52,53 +57,70 @@ impl HasColor for Point {
     }
 }
 
+#[cfg(test)]
+use alloc::boxed::Box;
+
+#[test]
+fn test_const() {
+    let test = Point {
+        id: 123,
+        x: 1,
+        y: 2,
+        col: 3,
+    };
+    let test_as_haslocation: &dyn HasLocation = &test;
+    assert_eq!(test_as_haslocation.location(), (1, 2));
+    assert_eq!(test_as_haslocation.id(), 123);
+    let test_cast = xdc::try_cast!(HasColor, test_as_haslocation).unwrap();
+    assert_eq!(test_cast.color(), 3);
+    assert_eq!(test_cast.id(), 123);
+}
+
+#[test]
+fn test_mut() {
+    let mut test = Point {
+        id: 123,
+        x: 1,
+        y: 2,
+        col: 3,
+    };
+    let test_as_haslocation: &mut dyn HasLocation = &mut test;
+    assert_eq!(test_as_haslocation.location(), (1, 2));
+    assert_eq!(test_as_haslocation.id(), 123);
+    let test_cast = xdc::try_cast_mut!(HasColor, test_as_haslocation).unwrap();
+    assert_eq!(test_cast.color(), 3);
+    assert_eq!(test_cast.id(), 123);
+}
+
+#[test]
+fn test_boxed() {
+    let test = Point {
+        id: 123,
+        x: 1,
+        y: 2,
+        col: 3,
+    };
+    let test_as_haslocation: Box<dyn HasLocation> = Box::new(test);
+    assert_eq!(test_as_haslocation.location(), (1, 2));
+    assert_eq!(test_as_haslocation.id(), 123);
+    let test_cast = xdc::try_cast_boxed!(HasColor, test_as_haslocation).unwrap();
+    assert_eq!(test_cast.color(), 3);
+    assert_eq!(test_cast.id(), 123);
+}
+
+#[test]
+fn test_bad_cast() {
+    let test = Point {
+        id: 123,
+        x: 1,
+        y: 2,
+        col: 3,
+    };
+    let test_as_haslocation: &dyn HasLocation = &test;
+    let test_cast = xdc::try_cast!(HasTaste, test_as_haslocation);
+    assert!(test_cast.is_none());
+}
+
 fn main() {
-    use alloc::boxed::Box;
-
-    {
-        let test = Point {
-            id: 123,
-            x: 1,
-            y: 2,
-            col: 3,
-        };
-        let test_as_haslocation: &dyn HasLocation = &test;
-        assert_eq!(test_as_haslocation.location(), (1, 2));
-        assert_eq!(test_as_haslocation.id(), 123);
-        let test_cast = xdc::try_cast!(HasColor, test_as_haslocation).unwrap();
-        assert_eq!(test_cast.color(), 3);
-        assert_eq!(test_cast.id(), 123);
-    }
-
-    {
-        let mut test = Point {
-            id: 123,
-            x: 1,
-            y: 2,
-            col: 3,
-        };
-        let test_as_haslocation: &mut dyn HasLocation = &mut test;
-        assert_eq!(test_as_haslocation.location(), (1, 2));
-        assert_eq!(test_as_haslocation.id(), 123);
-        let test_cast = xdc::try_cast_mut!(HasColor, test_as_haslocation).unwrap();
-        assert_eq!(test_cast.color(), 3);
-        assert_eq!(test_cast.id(), 123);
-    }
-
-    {
-        let test = Point {
-            id: 123,
-            x: 1,
-            y: 2,
-            col: 3,
-        };
-        let test_as_haslocation: Box<dyn HasLocation> = Box::new(test);
-        assert_eq!(test_as_haslocation.location(), (1, 2));
-        assert_eq!(test_as_haslocation.id(), 123);
-        let test_cast = xdc::try_cast_boxed!(HasColor, test_as_haslocation).unwrap();
-        assert_eq!(test_cast.color(), 3);
-        assert_eq!(test_cast.id(), 123);
-    }
-
     println!("Hello, world!");
 }
